95766c9df75d8999fef1d5b34428947fbb60cec2
elroydsouza2005@yahoo.co.uk <N0858835@ntu.ac.uk>
2024-10-09 21:26:44 +0100

    Added skeleton for report screen

---
+++ lib/screens/report_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ReportScreen extends StatelessWidget {
+  const ReportScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const Text("Report Screen"),
+      ],
+    );
+  }
+}//comment added to test new repo
