c23b3733125483b9991bad698826ec7fe530de75
elroydsouza2005@yahoo.co.uk <N0858835@ntu.ac.uk>
2024-10-09 21:38:25 +0100

    Copied formatting from homescreen

--- lib/screens/report_screen.dart
+++ lib/screens/report_screen.dart
@@ -5,10 +5,35 @@ class ReportScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const Text("Report Screen"),
-      ],
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromARGB(255, 255, 255, 255),
+              Color.fromARGB(255, 197, 197, 197),
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: const Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                'Report Form',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 600),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }//comment added to test new repo
