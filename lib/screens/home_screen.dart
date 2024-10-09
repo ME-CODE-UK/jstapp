83582ee47fbe35fe96beaa14c58404cebedba4c0
Max Beaumont <maxanderbeaumont@gmail.com>
2024-10-09 20:27:28 +0100

    added homescreen

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const Text("Title here"),
+        ElevatedButton(
+          onPressed: () {}, 
+          child: const Text("Send report"),
+        ),
+      ],
+    );
+  }
+}
