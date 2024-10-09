87819bbbdcf1d2a9815df949595e96664461f8d9
Max Beaumont <maxanderbeaumont@gmail.com>
2024-10-09 21:25:04 +0100

    Styled home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,14 +5,52 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const Text("Title here"),
-        ElevatedButton(
-          onPressed: () {}, 
-          child: const Text("Send report"),
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromARGB(255, 255, 255, 255),
+              Color.fromARGB(255, 197, 197, 197),
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
         ),
-      ],
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const Text(
+                'JST Forklift Report',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 30,
+                ),
+              ),
+              const SizedBox(height: 200),
+              Container(
+                width: 220,
+                height: 50,
+                child: ElevatedButton(    
+                  style: ElevatedButton.styleFrom(
+                    
+                    foregroundColor: Color.fromARGB(255, 255, 255, 255),
+                    backgroundColor: Color.fromARGB(255, 0, 0, 0),
+                  ),
+                  onPressed: () {},
+                  child: const Text(
+                    style: TextStyle(
+                      fontSize: 18,
+                    ),
+                    'Send report',
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }//comment added to test new repo
