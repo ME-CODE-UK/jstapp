fb577182a4738a14a7d7d086b2b57fc77de3a362
Max Beaumont <maxanderbeaumont@gmail.com>
2024-10-09 21:40:04 +0100

    Added google fonts library

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:jstapp/screens/report_screen.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
