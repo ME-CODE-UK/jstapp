7574b5e6d39899f69df03b53ee3baa1f3bfe8210
Max Beaumont <maxanderbeaumont@gmail.com>
2024-10-23 22:57:36 +0100

    added return to home screen and html support in email

--- lib/screens/report_screen.dart
+++ lib/screens/report_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:jstapp/screens/home_screen.dart';
 import 'package:mailer/mailer.dart';
 import 'package:mailer/smtp_server/gmail.dart';
 
@@ -268,9 +269,10 @@ class _ReportScreenState extends State<ReportScreen> {
                         ..recipients.add('mecodeuk@gmail.com')
                         ..ccRecipients.add(rBusEmail)
                         ..subject = 'Forklift App Report'
-                        ..text =
-                            'Forklift ID: $rID \nDriver name: $rDriver \nFault type: $_dropValue \n Report information: $rDetails';
+                        //..text =
+                            //'Forklift ID: $rID \nDriver name: $rDriver \nFault type: $_dropValue \n Report information: $rDetails';
                         // ..html = '<h1>Hello</h1>\n<p>Hey!</p><img src="cid:myimg">'
+                        ..html = '<h1>Report</h1><p><b>Forklift ID:</b> $rID<br><b>Driver name:</b> $rDriver<br><b>Fault type:</b> $_dropValue<br><b>Issue:</b> $rDetails</p>';
                         
                       for (File imageFile in attachments) {
                         message.attachments.add(FileAttachment(imageFile));
@@ -280,6 +282,8 @@ class _ReportScreenState extends State<ReportScreen> {
                       print('Message sent: ' + sendReport.toString());
                       ScaffoldMessenger.of(context).showSnackBar(
                           SnackBar(content: Text("Mail Sent Successfully")));
+
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => const HomeScreen()));
                     },
                     child: const Text(
                       style: const TextStyle(
